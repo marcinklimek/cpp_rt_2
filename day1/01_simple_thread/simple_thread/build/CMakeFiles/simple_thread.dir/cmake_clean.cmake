c8fab6061a8d5f1330f8891561172bd715bfd4a7
Marcin Klimek <git@marcinklimek.pl>
2014-04-07 10:01:54 +0200

    01

---
+++ day1/01_simple_thread/simple_thread/build/CMakeFiles/simple_thread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simple_thread.dir/main.cpp.o"
+  "simple_thread.pdb"
+  "simple_thread"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simple_thread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
